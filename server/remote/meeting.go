e569ebaab6d74708849a8c13cbb389a0d16364e0
Shivam Chauhan <shivam.chauhan@joshtechnologygroup.com>
2022-01-17 16:22:21 +0530

    MI-1603: Done review fixes

--- server/remote/meeting.go
+++ server/remote/meeting.go
@@ -7,35 +7,10 @@ type FindMeetingTimesParameters struct {
 	Attendees []Attendee `json:"attendees,omitempty"`
 }
 
-type TimeConstraint struct {
-	ActivityDomain string     `json:"activityDomain,omitempty"`
-	TimeSlots      []TimeSlot `json:"timeSlots,omitempty"`
-}
 type MeetingTimeSuggestion struct {
 	MeetingTimeSlot *DateTime `json:"meetingTimeSlot"`
 }
 
-type AttendeeAvailability struct {
-	Attendee     *Attendee
-	Availability string `json:"availability"`
-}
-
 type MeetingTimeSuggestionResults struct {
 	MeetingTimeSuggestions []*MeetingTimeSuggestion `json:"meetingTimeSuggestions"`
-}
-
-type TimeSlot struct {
-	Start *DateTime `json:"start,omitempty"`
-	End   *DateTime `json:"end,omitempty"`
-}
-
-type LocationConstraint struct {
-	Locations       []LocationConstraintItem `json:"locations,omitempty"`
-	IsRequired      *bool                    `json:"isRequired,omitempty"`
-	SuggestLocation *bool                    `json:"suggestLocation,omitempty"`
-}
-
-type LocationConstraintItem struct {
-	Location            *Location
-	ResolveAvailability *bool `json:"resolveAvailability,omitempty"`
 }
