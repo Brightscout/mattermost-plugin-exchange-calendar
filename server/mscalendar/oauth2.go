80f64d2eb01760040dbb5910eee28fd6c8ca03ea
Shivam Chauhan <shivam.chauhan@joshtechnologygroup.com>
2022-02-18 12:01:58 +0530

    MI-1623: Done review fixes

--- server/mscalendar/oauth2.go
+++ server/mscalendar/oauth2.go
@@ -19,10 +19,6 @@ const BotWelcomeMessage = "Bot user connected to account %s."
 const RemoteUserAlreadyConnected = "%s account `%s` is already mapped to Mattermost account `%s`. Please run `/%s disconnect`, while logged in as the Mattermost account"
 const RemoteUserAlreadyConnectedNotFound = "%s account `%s` is already mapped to a Mattermost account, but the Mattermost user could not be found"
 
-// type oauth2App struct {
-// 	Env
-// }
-
 type OAuth2 interface {
 	CompleteOAuth2(mattermostUserID string) error
 }
